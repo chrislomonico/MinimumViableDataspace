d91be773d15598e52caafa7b63b623f8bacebbb0
Marc Gomez <marcgomez@microsoft.com>
2022-07-14 16:16:35 +0200

    Fix integration test to verify remote IdentityHub when running on CI

--- system-tests/src/test/java/org/eclipse/dataspaceconnector/system/tests/identityhub/IdentityHubIntegrationTest.java
+++ system-tests/src/test/java/org/eclipse/dataspaceconnector/system/tests/identityhub/IdentityHubIntegrationTest.java
@@ -5,16 +5,22 @@ import okhttp3.OkHttpClient;
 import org.eclipse.dataspaceconnector.identityhub.client.IdentityHubClientImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.eclipse.dataspaceconnector.system.tests.utils.TestUtils.requiredPropOrEnv;
 
 public class IdentityHubIntegrationTest {
 
-    private static final String HUB_URL_FORMAT = "http://localhost:%s/api/identity-hub";
+    static final String PROVIDER_IDENTITY_HUB_URL = requiredPropOrEnv("PROVIDER_IDENTITY_HUB_URL", "http://localhost:8182/api/identity-hub");
+    static final String CONSUMER_EU_IDENTITY_HUB_URL = requiredPropOrEnv("CONSUMER_EU_IDENTITY_HUB_URL", "http://localhost:8182/api/identity-hub");
+    static final String CONSUMER_US_IDENTITY_HUB_URL = requiredPropOrEnv("CONSUMER_US_IDENTITY_HUB_URL", "http://localhost:8183/api/identity-hub");
 
     private static final OkHttpClient OK_HTTP_CLIENT = new OkHttpClient.Builder()
             .connectTimeout(1, TimeUnit.MINUTES)
@@ -31,13 +37,19 @@ public class IdentityHubIntegrationTest {
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {8181, 8182, 8183})
-    void retrieveVerifiableCredentials_empty(int hubPort) throws IOException {
-        var hubUrl = String.format(HUB_URL_FORMAT, hubPort);
-
+    @MethodSource("provideHubUrls")
+    void retrieveVerifiableCredentials_empty(String hubUrl) throws IOException {
         var vcs = client.getVerifiableCredentials(hubUrl);
 
         assertThat(vcs.succeeded()).isTrue();
         assertThat(vcs.getContent()).isEmpty();
+    }
+
+    private static Stream<Arguments> provideHubUrls() {
+        return Stream.of(
+                Arguments.of(PROVIDER_IDENTITY_HUB_URL),
+                Arguments.of(CONSUMER_EU_IDENTITY_HUB_URL),
+                Arguments.of(CONSUMER_US_IDENTITY_HUB_URL)
+        );
     }
 }
