82488171824f3cecc2632594494212cafbbe7770
Alexandre Gattiker <algattik@microsoft.com>
2022-07-19 07:30:16 +0200

    Update BlobTransferIntegrationTest.java

--- system-tests/src/test/java/org/eclipse/dataspaceconnector/system/tests/local/BlobTransferIntegrationTest.java
+++ system-tests/src/test/java/org/eclipse/dataspaceconnector/system/tests/local/BlobTransferIntegrationTest.java
@@ -64,6 +64,7 @@ public class BlobTransferIntegrationTest {
 
     @NotNull
     private BlobServiceClient getBlobServiceClient(String keyVaultName) {
+        // Not using AzureCliCredentialBuilder because of agent issue https://github.com/orgs/github-community/discussions/20830
         var credential = new AzureCliCredentialBuilder().build();
         var vault = new SecretClientBuilder()
                 .vaultUrl(format(KEY_VAULT_ENDPOINT_TEMPLATE, keyVaultName))
